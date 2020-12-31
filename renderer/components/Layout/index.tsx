abd35081b112525fa84ec4318c1304d4c9c2c6aa
Keven Leone <keven.santos.sz@gmail.com>
2020-12-31 15:57:12 -0300

    style: add eslint

--- renderer/components/Layout/index.tsx
+++ renderer/components/Layout/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
-export default function index({children}) {
-    return (
-        <div className='application_layout'>
-            <div className="background">
-                <div className='application_container'>
-                    {children}
-                </div>
-            </div>
+export default function index ({ children }) {
+  return (
+    <div className="application_layout">
+      <div className="background">
+        <div className="application_container">
+          {children}
         </div>
-    )
+      </div>
+    </div>
+  )
 }
--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import Layout from '../components/Layout'
-import { Button } from 'react-bootstrap'
+import Layout from '../components/Layout';
+import { Button } from 'react-bootstrap';
 
 export default function home() {
     return (
